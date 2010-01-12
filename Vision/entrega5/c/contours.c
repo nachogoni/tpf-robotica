6643b294c64658ce390602280edd65d01b3ca58a
NulDiego <NulDiego@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-01-12 04:04:12 +0000

    agregado algunos filtros

--- Vision/entrega5/c/contours.c
+++ Vision/entrega5/c/contours.c
@@ -71,3 +71,90 @@ void printContour(CvSeq * contour, int depthLevel, CvScalar color,IplImage * dst
 }
 
 
+/*Contour Filters
+ *
+ */
+
+int
+areaFilter(CvSeq * contour,double min_area,double max_area){
+	double area;
+	area=fabs(cvContourArea(contour,CV_WHOLE_SEQ));
+	
+	return area> min_area && area<max_area;
+	
+}
+
+int
+perimeterFilter(CvSeq * contour,double min_per,double max_per){
+	
+	double per;
+	per=cvContourPerimeter(contour);
+	
+	return per> min_per && per<max_per;	
+	
+}
+
+int
+rectangularAspectFilter(CvSeq * contour,double minWidthHeightRatio,
+double maxWidthHeightRatio ){
+	CvBox2D box;
+	CvMemStorage* mem = cvCreateMemStorage(0);
+	int ret;
+	
+	box=cvMinAreaRect2(contour,mem);
+	
+	if( (box.size.width > minWidthHeightRatio* box.size.height) &&
+	 (box.size.width < (maxWidthHeightRatio)* box.size.height) || 
+	 (box.size.height > minWidthHeightRatio* box.size.width) &&
+     (box.size.height < (maxWidthHeightRatio)*box.size.width)){
+		  ret=1;
+	 }
+	 else{
+		ret=0;	
+	}
+	 
+	 cvReleaseMemStorage( &mem );
+	 return ret;
+}
+
+int
+boxAreaFilter(CvSeq * contour,double minAreaRatio){
+	CvBox2D box;
+	CvMemStorage* mem = cvCreateMemStorage(0);
+	double boxArea,contourArea,dif,ret;
+	
+	box=cvMinAreaRect2(contour,mem);
+	boxArea=box.size.width*box.size.height;
+	contourArea=fabs(cvContourArea(contour,CV_WHOLE_SEQ));
+	dif=boxArea-contourArea;
+	ret=(1- (dif/boxArea));
+        
+     
+    cvReleaseMemStorage( &mem );
+	return ret > minAreaRatio;
+}
+
+int
+histogramMatchingFilter(IplImage * frame,CvHistogram * testImageHistogram,
+double min){
+	//~ CvBox2d box;
+	//~ CvMemStorage* mem = cvCreateMemStorage(0);
+	//~ 
+	//~ 
+	//~ box=cvMinAreaRect2(contour,mem);
+	//~ void cvSetImageROI( IplImage* image, CvRect rect );
+//~ 
+	//~ 
+	//~ 
+	//~ 
+	
+	
+	return 0;
+	
+	
+
+
+}
+
+
+
