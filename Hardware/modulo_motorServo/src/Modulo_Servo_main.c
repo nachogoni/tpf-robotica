c81367767e3d39ef9d1632311951b8fc53b3bdcb
nachogoni <nachogoni@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-02-04 21:40:52 +0000

    funcional - pero no aceptable.

---
+++ Hardware/modulo_motorServo/src/Modulo_Servo_main.c
@@ -0,0 +1,259 @@
+#define VERSION "0.1"
+
+/* Modulo Motor - main.c
+ * PIC16F88 - MAX232 - SERVO
+ *
+ *                               PIC16F88
+ *                .------------------------------------.
+ *  SOFTWARE_PWM -|RA2/AN2/CVREF/VREF           RA1/AN1|- 
+ *           LED -|RA3/AN3/VREF+/C1OUT          RA0/AN0|- 
+ *           LED -|RA4/AN4/T0CKI/C2OUT    RA7/OSC1/CLKI|- XT CLOCK pin1, 27pF to GND
+ * RST/ICD2:MCLR -|RA5/MCLR/VPP           RA6/OSC2/CLKO|- XT CLOCK pin2, 27pF to GND
+ *           GND -|VSS                              VDD|- +5v
+ *  HARDWARE_PWM -|RB0/INT/CCP1       RB7/AN6/PGD/T1OSI|- ICD2:PGD
+ *               -|RB1/SDI/SDA  RB6/AN5/PGC/T1OSO/T1CKI|- ICD2:PGC
+ *  MAX232:R1OUT -|RB2/SDO/RX/DT           RB5/SS/TX/CK|- MAX232:T1IN
+ *      ICD2:PGM -|RB3/PGM/CCP1             RB4/SCK/SCL|- 
+ *                '------------------------------------'
+ *    
+ */
+
+#include <16F88.h>
+#include <stdio.h>
+#include <stdlib.h>
+#fuses HS,NOWDT,NOPROTECT,NOLVP
+#use delay (clock=20000000)
+
+#use rs232(BAUD=115200,PARITY=N,XMIT=PIN_B5,RCV=PIN_B2,BITS=8,ERRORS,TIMEOUT=1,STOP=1,UART1)
+#use fast_io(A)
+#use fast_io(B)
+
+#byte porta=0x05
+#byte portb=0x06
+
+// Led
+#bit led1=porta.3
+#bit led2=porta.4
+// PWMs
+#bit pwm1=porta.2
+#bit pwm2=porta.1 // TODO: select another pin!
+#bit pwm3=porta.1 // TODO: select another pin!
+#bit pwm4=porta.1 // TODO: select another pin!
+#bit pwm5=porta.1 // TODO: select another pin!
+// MAX232
+#bit tx=portb.5
+#bit rx=portb.2
+
+#define CARD_GROUP		0x01
+#define CARD_ID			0x01
+
+// Buffer del pto serial
+#define MAX_BUFFER_SIZE	50
+char buffer[MAX_BUFFER_SIZE];
+int buffer_idx = 0;
+
+// Buffer de respuesta
+char resp[30];
+int resp_idx = 0;
+
+// Valor de duty del PWM
+signed long duty = 0;
+
+signed long value = 0;
+signed long dutyv = 0;
+int change = 1;
+
+short rs232c = 0;
+char c = 0;
+int command_size = 6;
+
+/* Examina y ejecula el comando */
+void command(char * cmd, int size);
+/* Setea el PWM */
+void SetPWM(signed long pwm);
+/* Envia los datos por el pto serial */
+void send(char * response, int size);
+
+// Interrupcion del Timer0
+#INT_RTCC
+void Timer0_INT()
+{
+	// Seteo el valor para que interrumpa cada 6.25ms
+    set_timer0(250);
+
+	pwm1++;
+	
+	return;
+}
+
+void main()
+{
+	// Control de Velocidad comandado por RS232
+
+	set_tris_a(0b11100011);  // TODO: check!
+	set_tris_b(0b11100110);
+	
+	// ***PWM***
+	setup_ccp1(CCP_PWM);
+	// Seteo al PWM con f: 4.88 kHz, duty = 0
+	set_pwm1_duty(0);
+	setup_timer_2(T2_DIV_BY_16, 255, 1);
+
+	// ***TIMER0 - TIME BASE***
+	// Seteo el Timer0 como clock -> dt = 6.25ms
+	setup_timer_0(RTCC_INTERNAL | RTCC_DIV_1);
+	set_timer0(250);
+
+	// Interrupcion sobre el Timer0
+	//enable_interrupts(INT_TIMER1);
+	enable_interrupts(INT_RDA);
+
+	// Habilito las interrupciones
+	enable_interrupts(GLOBAL);
+	
+	setPWM(0);
+	
+	dutyv = 168;
+	
+	// FOREVER
+	while(1)
+	{
+	
+	if (change-- > 0)
+	{
+		// PWM x software
+		delay_us(5);
+
+		if (value == 0) {
+			pwm1 = 1;
+		}
+		
+		if (value >= 40 && value == dutyv) {
+			pwm1 = 0;
+//		} else if (value == 250) {
+//			pwm1 = 0;
+		} else if (value >= 1710) {
+//		} else if (value == 15010) {
+			value = -1;
+			change = 0;
+		}
+		
+		value++;
+	}	
+
+	        // Mini consola
+	        if (rs232c == 1)
+	        {
+	                rs232c = 0;
+	                switch (c)
+	                {
+	                        case 0:
+	                                break;
+	                        case 13:
+	                                // Ingreso un comando...
+	                                buffer[buffer_idx] = '\0';
+	                                command(buffer, 0);
+	                                buffer_idx = 0;
+	                                break;
+	                        case 8:
+	                                // Backspace
+	                                if (buffer_idx > 0)
+	                                        buffer_idx--;
+	                                break;
+	                        case '*':
+	                                // Pido el valor del Timer0
+	                                printf("\n\rTimer0: %d\n\r", get_timer0());
+	                                break;
+	                        default:
+	                                // Otro caracter
+	                                if (buffer_idx <= MAX_BUFFER_SIZE)
+	                                        buffer[buffer_idx++] = c;
+	                                break;                          
+	                }
+	        }
+	}
+
+
+	return;
+}
+
+// Interrupcion del RS232
+#INT_RDA
+void RS232()
+{
+	// Agrego al buffer el caracter
+	c = getc();
+	rs232c = 1;
+	putc(c);
+	return;
+}
+
+/* Verifica que el comando sea valido y lo ejecuta */
+void command(char * cmd, int size)
+{
+        // TODO: Crear protocolo
+        if (cmd[0] == '+')
+        {
+	        duty++;
+	        if (duty > 1023)
+	        	duty = 1023;
+			setPWM(duty);
+			printf("\rDuty a: %ld", duty);
+        } else 
+        if (cmd[0] == '-')
+        {
+	        duty--;
+	        if (duty < 0)
+	        	duty = 0;
+			setPWM(duty);
+			printf("\rDuty a: %ld", duty);
+        } else 
+        if (cmd[0] == 'd')
+        {
+            duty = atol(cmd + 1);
+	        if (duty < 0)
+	        	duty = 0;
+	        if (duty > 1023)
+	        	duty = 1023;
+			setPWM(duty);
+			printf("\rDuty a: %ld", duty);
+        }
+        
+        change = 10;
+        dutyv = duty;
+        
+        return; 
+}  	
+
+/* Envia los datos por el pto serial */
+void send(char * response, int size)
+{
+	int i, checksum = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		// Calcular el crc
+		checksum ^= response[i];
+		putc(response[i]);
+	}
+	
+	// Enviar el CRC
+	putc(checksum);
+	
+	return;	
+}	
+
+/* Setea el duty del PWM segun el valor. Positivo o negativo determina el sentido */
+void setPWM(signed long pwm)
+{
+  long pset;
+ 
+  pset = (abs(pwm));
+
+  if (pset > 1023L)
+    pset = 1023;
+
+  set_pwm1_duty(pset);
+  
+  return;
+}
