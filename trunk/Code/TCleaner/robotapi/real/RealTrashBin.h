84c32ac45a5f8f66fa21a53da8f3d9f095959795
guicamest <guicamest@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-02-19 22:30:19 +0000

    Added TrashBin Class

---
+++ trunk/Code/TCleaner/robotapi/real/RealTrashBin.h
@@ -0,0 +1,15 @@
+#ifndef robotapi_real_RealTrashBin_h
+#define robotapi_real_RealTrashBin_h
+
+#include <robotapi/ITrashBin.h>
+
+
+namespace robotapi {
+namespace real {
+
+class RealTrashBin : virtual public robotapi::ITrashBin {};
+
+} /* End of namespace robotapi::real */
+} /* End of namespace robotapi */
+
+#endif // robotapi_real_RealTrashBin_h
