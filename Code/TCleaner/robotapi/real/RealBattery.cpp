15e09aefc06671740aefab58d7c17e60bde3d495
guicamest <guicamest@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-02-20 00:37:32 +0000

    First Real Sensor COMMITTED!!!!

--- Code/TCleaner/robotapi/real/RealBattery.cpp
+++ Code/TCleaner/robotapi/real/RealBattery.cpp
@@ -3,5 +3,37 @@
 namespace robotapi {
 namespace real {
 
+	RealBattery::RealBattery(protocol::handlers::BatteryBoardPacketHandler * bbph){
+		this->bbph = bbph;
+	}
+
+	void RealBattery::enable(int ms){
+		return;
+	}
+
+	void RealBattery::disable(){
+		return;
+	}
+
+	double RealBattery::getValue(){
+		return this->bbph->getValue();
+	}
+
+	bool RealBattery::isFull(){
+		return this->bbph->isFull();
+	}
+
+	void RealBattery::setEmptyBias(double bias){
+		this->bbph->setEmptyBias(bias);
+	}
+
+	bool RealBattery::isEmpty(){
+		return this->bbph->isEmpty();
+	}
+
+	void RealBattery::setFullBias(double bias){
+		this->bbph->setFullBias(bias);
+	}
+
 } /* End of namespace robotapi::real */
 } /* End of namespace robotapi */
--- Code/TCleaner/robotapi/real/RealBattery.h
+++ Code/TCleaner/robotapi/real/RealBattery.h
@@ -2,13 +2,35 @@
 #define robotapi_real_RealBattery_h
 
 #include "robotapi/IBattery.h"
-
+#include <protocol/handlers/BatteryBoardPacketHandler.h>
 
 
 namespace robotapi {
 namespace real {
 
-class RealBattery : virtual public robotapi::IBattery {};
+class RealBattery : virtual public robotapi::IBattery {
+
+	public:
+		RealBattery::RealBattery(protocol::handlers::BatteryBoardPacketHandler * bbph);
+
+		void enable(int ms);
+
+		void disable();
+
+		double getValue();
+
+		bool isFull();
+
+		void setEmptyBias(double bias);
+
+		bool isEmpty();
+
+		void setFullBias(double bias);
+
+	private:
+		protocol::handlers::BatteryBoardPacketHandler * bbph;
+
+	};
 
 } /* End of namespace robotapi::real */
 } /* End of namespace robotapi */
