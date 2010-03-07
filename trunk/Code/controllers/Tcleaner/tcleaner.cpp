18566f615cb70bb3915cfb8a13917ccd310a29b1
guicamest <guicamest@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-03-07 16:08:50 +0000

    Webots compiling and linking with opencv libraries and garbage recognition code. Functionality not tested yet

--- trunk/Code/controllers/Tcleaner/tcleaner.cpp
+++ trunk/Code/controllers/Tcleaner/tcleaner.cpp
@@ -9,17 +9,20 @@
 #include <robotapi/Webts/WebotsRobot.h>
 #include <robotapi/Webts/WebotsDifferentialWheels.h>
 #include <webots/DifferentialWheels.hpp>
-#include "GarbageCleaner.h"
+#include <GarbageCleaner.h>
+#include <cv.h>
+#include <highgui.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+
 	webots::DifferentialWheels * wdw = new webots::DifferentialWheels();
     robotapi::webts::WebotsRobot * robot = new robotapi::webts::WebotsRobot(*wdw);
 
 	GarbageCleaner::GarbageCleaner * gc = new GarbageCleaner::GarbageCleaner(*robot);
 	gc->cleanGarbage();
-
+  
     return EXIT_SUCCESS;
 }
