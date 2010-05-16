ce49eb6e3a8b4209063abf528df9c98ed71920f4
NulDiego <NulDiego@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-05-16 19:39:08 +0000

    :wq asd

---
+++ trunk/Vision/benchmarker/cpp/compilar_algv1.sh
@@ -0,0 +1,2 @@
+g++ -g -lcv -lhighgui benchmark.cpp Frame.cpp Cobject.cpp xmlParser.cpp Result.cpp GarbageAdapter.cpp algv1/GarbageRecognition.cpp  alg8/Contours.cpp algv1/Histogram.cpp algv1/Garbage.cpp algv1/MinimalBoundingRectangle.cpp -o test_algv1 
+
