c4bab20899d8899b2523feea0c8c803857b76a83
nuldiego <nuldiego@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-07-14 08:36:13 +0000

    platos ready, faltaria integrarlo con el common

--- Vision/benchmarker/cpp/algPlatos/Contours.cpp
+++ Vision/benchmarker/cpp/algPlatos/Contours.cpp
@@ -240,6 +240,42 @@ traversePoints(CvSeq * contour,IplImage * img){
 		
 		
 	}
+	
+	double angle( CvPoint* pt1, CvPoint* pt2, CvPoint* pt0 )
+{
+    double dx1 = pt1->x - pt0->x;
+    double dy1 = pt1->y - pt0->y;
+    double dx2 = pt2->x - pt0->x;
+    double dy2 = pt2->y - pt0->y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+double traverseAngles(CvSeq * points){
+	int i;
+	double s,t;
+	s=0;
+	printf("\n-- angulos de  %d---\n",points->total);
+	for( i = 0; i < points->total +1 ; i++ )
+	{
+		// find minimum angle between joint
+		// edges (maximum of cosine)
+		if( i >= 2 )
+		{
+			t = angle(
+			(CvPoint*)cvGetSeqElem( points, i ),
+			(CvPoint*)cvGetSeqElem( points, i-2 ),
+			(CvPoint*)cvGetSeqElem( points, i-1 ));
+			printf("%g |", t);
+			s = s > t ? s : t;
+		}
+		if(t>0){
+			printf("angulo sd");
+			
+		}
+			
+	}	
+}
+
 
 namespace utils{
 
@@ -285,6 +321,7 @@ void Contours::printContour(int depthLevel, CvScalar color,IplImage * dst){
 	
 	CvFont font;
 	int line_type=CV_AA;
+	CvBox2D box2;
 	
 	char * a=(char *) malloc(30);
 	char * b=(char *) malloc(30);
@@ -298,7 +335,8 @@ void Contours::printContour(int depthLevel, CvScalar color,IplImage * dst){
 	
 	CvMemStorage* mem = cvCreateMemStorage(0);
 	CvBox2D box=cvMinAreaRect2(this->c,mem);
-	CvBox2D box2=cvFitEllipse2(this->c);
+	if(this->c->total>6)
+		box2=cvFitEllipse2(this->c);
 	
 	CvPoint *longestEdge=getLongestEdge(this->c);
 	double l1=edgeLength(longestEdge[0],longestEdge[1]);
@@ -314,8 +352,9 @@ void Contours::printContour(int depthLevel, CvScalar color,IplImage * dst){
 	//~ sprintf(a,"difArea: %d -%d", (int) box.size.width,(int)box.size.height);
 	double area=this->getArea();
 	double areaPercent=(fabs(PI*(box.size.width/2)*(box.size.height/2) - area));
-	sprintf(a,"l1/p %g" ,l1/this->getPerimeter());
-	sprintf(b,"areaDIf %g",areaPercent);
+	double angles=traverseAngles(this->c);
+	sprintf(a,"angles %g" ,angles);
+	sprintf(b,"areaDIf %g",areaPercent/area);
 	double majorAxis,minorAxis;
 	double majorAxis2,minorAxis2;
 	
@@ -338,14 +377,14 @@ void Contours::printContour(int depthLevel, CvScalar color,IplImage * dst){
 	sprintf(c,"ecc: %g",sqrt(majorAxis*majorAxis -minorAxis*minorAxis)/majorAxis);
 	sprintf(d,"circularity %g",this->getPerimeter()*this->getPerimeter()/this->getArea());
 	double dif=fabs(majorAxis - majorAxis2) + fabs(minorAxis - minorAxis2);
-	sprintf(e,"dif box: %g",dif);
+	sprintf(e,"dif box: %g",dif/(minorAxis+majorAxis));
 	
 	cvInitFont( &font, CV_FONT_HERSHEY_COMPLEX, 0.5, 0.5, 0.0,0.5, line_type );
-	cvPutText( dst, a, pt2, &font, CV_RGB(255,255,0));
-	cvPutText( dst, c, pt3, &font, CV_RGB(255,255,0));
-	cvPutText( dst, b, pt4, &font, CV_RGB(255,255,0));
-	cvPutText( dst, d, pt5, &font, CV_RGB(255,255,0));
-	cvPutText( dst, e, pt6, &font, CV_RGB(255,255,0));
+	cvPutText( dst, a, pt2, &font, CV_RGB(0,0,0));
+	cvPutText( dst, c, pt3, &font, CV_RGB(0,0,0));
+	cvPutText( dst, b, pt4, &font, CV_RGB(0,0,0));
+	cvPutText( dst, d, pt5, &font, CV_RGB(0,0,0));
+	cvPutText( dst, e, pt6, &font, CV_RGB(0,0,0));
 
 	//~ free(a);
 	cvReleaseMemStorage(&mem);
@@ -420,13 +459,14 @@ int Contours::rectangularAspectFilter(double minWidthHeightRatio, double maxWidt
 int Contours::platoFilter(){
 	
 	CvMemStorage* mem = cvCreateMemStorage(0);
+	
+	if(this->c->total<6)
+		return false;
+	
+	
+		
 	CvBox2D box=cvMinAreaRect2(this->c,mem);
 	CvBox2D box2=cvFitEllipse2(this->c);
-	
-	double circ=this->getEccentricity();
-	
-	if(circ<13 || circ>18)
-		return false;
 	
 	double majorAxis,minorAxis;
 	double majorAxis2,minorAxis2;
@@ -449,16 +489,29 @@ int Contours::platoFilter(){
 	}
 	
 	double boxDiff=fabs(majorAxis - majorAxis2) + fabs(minorAxis-minorAxis2);
-	
-	
-	
-	
+	//eccentricity formula
 	double ecc=sqrt(majorAxis*majorAxis -minorAxis*minorAxis)/majorAxis;
+	
+	
+	//~ if(ecc>0.95)
+		//~ return false;
+	//estimated area
+	
 	double calcArea=PI*(majorAxis/2)*(minorAxis/2);
+	double realArea=this->getArea();
 	
+	if(fabs(calcArea-realArea)/realArea > 0.2)
+		return false;
 	
+	double circ=this->getEccentricity();
 	
-	return false;
+	if(circ<10 || circ>20)
+		return false;
+	
+	if(ecc<0.65 || ecc>0.95)
+		return false;
+	
+	return true;
 	
 	
 }
@@ -638,6 +691,21 @@ int Contours::vasoFilter(){
 	return true;
 }
 
+
+
+    //~ for( i = 0; i < 5; i++ )
+                    //~ {
+                        //~ // find minimum angle between joint
+                        //~ // edges (maximum of cosine)
+                        //~ if( i >= 2 )
+                        //~ {
+                            //~ t = fabs(angle(
+                            //~ (CvPoint*)cvGetSeqElem( result, i ),
+                            //~ (CvPoint*)cvGetSeqElem( result, i-2 ),
+                            //~ (CvPoint*)cvGetSeqElem( result, i-1 )));
+                            //~ s = s > t ? s : t;
+                        //~ }
+                    //~ }
 
 
 
