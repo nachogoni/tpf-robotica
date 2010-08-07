7d7421210e707eafdab8f5ef70870ff796203a99
guicamest <guicamest@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-08-07 04:00:37 +0000

    Going forward

--- Tesis/comportamientos/allPlot.m
+++ Tesis/comportamientos/allPlot.m
@@ -1,13 +1,16 @@
 
-plot(unload,'1*;Descargar Basura;')
+hold off;
+legend('','location','northwest');
 hold on;
-plot(wander,'3*;Wandering;');
-plot(recharge,'2*;Recargar Bateria;')
-plot(avoid,'5;Evitar obstaculos;')
-plot(goto,'0;Ir a Basura;');
-plot(collect,'1;Recolectar;')
-plot(focus,'2;Enfocar;');
+plot(unload,'1;Descargar Basura;',"linewidth",1,"linestyle","--")
+xlabel "Cantidad de steps de simulacion";
+ylabel "Cantidad de steps activo";
+title "Progreso de todos los comportamientos";
+plot(wander,'3;Wandering;',"linewidth",2);
+plot(recharge,'2;Recargar Bateria;',"linewidth",3);
+plot(avoid,'5;Evitar obstaculos;',"linewidth",4);
+plot(goto,';Ir a Basura;',"linewidth",5);
+plot(collect,'1;Recolectar;',"linewidth",6);
+plot(focus,'2;Enfocar;',"linewidth",7,"linestyle","-.");
 print('graphics/All.png','-dpng');
-xlabel "Steps";
-ylabel "# de steps activo";
 hold off;
--- Tesis/comportamientos/plotPie.m
+++ Tesis/comportamientos/plotPie.m
@@ -1,13 +1,17 @@
 
-names = {"Wandering","Enfocar basura", "Ir a basura","Recolectar basura", "Descargar basura","Recargar bateria", "Evitar obstaculos", "Salir situaciones no deseadas"};
+clear labels
+bnames = {"Wandering","Enfocar basura", "Ir a basura","Recolectar basura", "Descargar basura","Recargar bateria", "Evitar obstaculos", "Salir situaciones no deseadas"};
 
 i=1;
-
-while( i < 9 )
-	labels{i} = sprintf("%s (%.2g)", names{i}, percentages(i));
+while( i <= length(bnames) )
+	labels{i} = sprintf("%s (%.2g)", bnames{i}, percentages(i));
 	i++;
 endwhile
 
-pie(vars(length(vars),:),[0,0,0,0,1,0,0,0],labels)
+h = pie(vars(length(vars),:),[0,0,0,0,1,0,0,0],labels)
+%set(h(4),'color',aux2);
+set(h(3),'facecolor',[1 1 0]);
+%set(h(12),'color',[0.5 0.5 0.5]');
 axis([-3.4 3.4]);
+replot()
 print('graphics/BehaviourDistributionPie.png','-dpng');
