05b943fa389e4bbd55030a007898c4e53b95f97d
nuldiego <nuldiego@d69ea68a-f96b-11de-8cdf-e97ad7d0f28a>
2010-08-25 00:16:53 +0000

    Added BehaviourParameters utility class

---
+++ trunk/Code/controllers/RecollectionBenchmark/behaviours/BehavioursParameters.cpp
@@ -0,0 +1,43 @@
+#include "BehavioursParameters.h"
+#include <string>
+using namespace std;
+
+namespace behaviours{
+	
+struct ltstr
+{
+  bool operator()(const char* s1, const char* s2) const
+  {
+    return strcmp(s1, s2) < 0;
+  }
+};
+	
+		BehavioursParameters::BehavioursParameters(FILE * config)
+		{
+			char  variablename [200] = {0};
+			double variablevalue;
+			int ret = fscanf (config, "%s %lf\n", variablename, &variablevalue);
+			while ( ret != -1 ){
+				if ( ret == 2 ){
+					string* aux=new string ( variablename);
+					char * a=(char*)aux->c_str();
+					parameters[a] = variablevalue;
+					
+				}
+				ret = fscanf (config, "%s %lf\n", variablename, &variablevalue);
+			}
+			
+		}
+
+		BehavioursParameters::~BehavioursParameters(){}
+		
+		double BehavioursParameters::getParameter(char * param){
+			return this->parameters[param];
+		}
+		
+		void BehavioursParameters::setParameter(char * param,double val){
+			parameters[param] = val;
+		}
+
+
+}
